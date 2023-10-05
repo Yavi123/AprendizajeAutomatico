92279e1ef41f702a28a77ce7f9e3fe365f64adf4
Javier V <javilleg@ucm.es>
2023-10-05 17:40:58 +0200

    coste

--- Practica1/Practica/linear_reg.py
+++ Practica1/Practica/linear_reg.py
@@ -7,36 +7,56 @@ import math
 # Cost function
 #
 def compute_cost(x, y, w, b):
-    """
-    Computes the cost function for linear regression.
+    # """
+    # Computes the cost function for linear regression.
 
-    Args:
-        x (ndarray): Shape (m,) Input to the model (Population of cities)
-        y (ndarray): Shape (m,) Label (Actual profits for the cities)
-        w, b (scalar): Parameters of the model
+    # Args:
+    #     x (ndarray): Shape (m,) Input to the model (Population of cities)
+    #     y (ndarray): Shape (m,) Label (Actual profits for the cities)
+    #     w, b (scalar): Parameters of the model
 
-    Returns
-        total_cost (float): The cost of using w,b as the parameters for linear regression
-               to fit the data points in x and y
-    """
+    # Returns
+    #     total_cost (float): The cost of using w,b as the parameters for linear regression
+    #            to fit the data points in x and y
+    # """
+
+    m = np.size(x)
+
+    res = 0
+
+    for i in range(0, m):
+        res += (f_wb(w, b, x[i]) - y[i]) ** 2
+
+    total_cost = res / (2 * m)
 
     return total_cost
+
+def f_wb(w, b, x):
+    return w * x + b
 
 
 #########################################################################
 # Gradient function
 #
 def compute_gradient(x, y, w, b):
-    """
-    Computes the gradient for linear regression 
-    Args:
-      x (ndarray): Shape (m,) Input to the model (Population of cities) 
-      y (ndarray): Shape (m,) Label (Actual profits for the cities)
-      w, b (scalar): Parameters of the model  
-    Returns
-      dj_dw (scalar): The gradient of the cost w.r.t. the parameters w
-      dj_db (scalar): The gradient of the cost w.r.t. the parameter b     
-     """
+    # """
+    # Computes the gradient for linear regression 
+    # Args:
+    #   x (ndarray): Shape (m,) Input to the model (Population of cities) 
+    #   y (ndarray): Shape (m,) Label (Actual profits for the cities)
+    #   w, b (scalar): Parameters of the model  
+    # Returns
+    #   dj_dw (scalar): The gradient of the cost w.r.t. the parameters w
+    #   dj_db (scalar): The gradient of the cost w.r.t. the parameter b     
+    #  """
+
+    m = len(x)
+    predictions = np.dot(x, w) + b
+    error = predictions - y
+
+    dj_dw = (1 / m) * np.dot(error, x)
+
+    dj_db = (1 / m) * np.sum(error)
 
     return dj_dw, dj_db
 
@@ -45,25 +65,25 @@ def compute_gradient(x, y, w, b):
 # gradient descent
 #
 def gradient_descent(x, y, w_in, b_in, cost_function, gradient_function, alpha, num_iters):
-    """
-    Performs batch gradient descent to learn theta. Updates theta by taking 
-    num_iters gradient steps with learning rate alpha
+    # """
+    # Performs batch gradient descent to learn theta. Updates theta by taking 
+    # num_iters gradient steps with learning rate alpha
 
-    Args:
-      x :    (ndarray): Shape (m,)
-      y :    (ndarray): Shape (m,)
-      w_in, b_in : (scalar) Initial values of parameters of the model
-      cost_function: function to compute cost
-      gradient_function: function to compute the gradient
-      alpha : (float) Learning rate
-      num_iters : (int) number of iterations to run gradient descent
-    Returns
-      w : (ndarray): Shape (1,) Updated values of parameters of the model after
-          running gradient descent
-      b : (scalar) Updated value of parameter of the model after
-          running gradient descent
-      J_history : (ndarray): Shape (num_iters,) J at each iteration,
-          primarily for graphing later
-    """
+    # Args:
+    #     x :    (ndarray): Shape (m,)
+    #     y :    (ndarray): Shape (m,)
+    #     w_in, b_in : (scalar) Initial values of parameters of the model
+    #     cost_function: function to compute cost
+    #     gradient_function: function to compute the gradient
+    #     alpha : (float) Learning rate
+    #     num_iters : (int) number of iterations to run gradient descent
+    # Returns
+    #     w : (ndarray): Shape (1,) Updated values of parameters of the model after
+    #         running gradient descent
+    #     b : (scalar) Updated value of parameter of the model after
+    #         running gradient descent
+    #     J_history : (ndarray): Shape (num_iters,) J at each iteration,
+    #         primarily for graphing later
+    # """
 
     return w, b, J_history
