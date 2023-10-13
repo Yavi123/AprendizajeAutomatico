5776249dad56d0cf6f3a8418249ed8fd70a3c0a5
NachoDelCastillo <nachodelcastillorubio@gmail.com>
2023-10-13 14:01:22 +0200

    Practica 1 terminada

--- Practica1/Practica/linear_reg.py
+++ Practica1/Practica/linear_reg.py
@@ -3,10 +3,12 @@ import copy
 import math
 
 
+
+
 #########################################################################
 # Cost function
 #
-def compute_cost(x, y, w, b):    
+def compute_cost(x, y, w, b):
     m = np.size(x)
 
     # Calcular las predicciones en Y
@@ -26,13 +28,16 @@ def compute_cost(x, y, w, b):
 # Gradient function
 #
 def compute_gradient(x, y, w, b):
+    # Numero de puntos
     m = len(x)
-    predictions = np.dot(x, w) + b
-    error = predictions - y
+    # linearRegressionValues almacena que valor en y deberian tener los numeros aleatorios si estuvieran en la recta
+    linearRegressionValues = (x * w) + b
+    # La diferencia de distancia entre los puntos aleatorios (x, y) y los calculados dentro de la recta
+    dif = linearRegressionValues - y
 
-    dj_dw = (1 / m) * np.dot(error, x)
-
-    dj_db = (1 / m) * np.sum(error)
+    # aplicamos la derivada de la funcion de coste anterior
+    dj_dw = (1 / m) * np.sum(dif * x)
+    dj_db = (1 / m) * np.sum(dif)
 
     return dj_dw, dj_db
 
@@ -41,41 +46,27 @@ def compute_gradient(x, y, w, b):
 # gradient descent
 #
 def gradient_descent(x, y, w_in, b_in, cost_function, gradient_function, alpha, num_iters):
-    """
-    Performs batch gradient descent to learn theta. Updates theta by taking 
-    num_iters gradient steps with learning rate alpha
-
-    Args:
-      x :    (ndarray): Shape (m,)
-      y :    (ndarray): Shape (m,)
-      w_in, b_in : (scalar) Initial values of parameters of the model
-      cost_function: function to compute cost
-      gradient_function: function to compute the gradient
-      alpha : (float) Learning rate
-      num_iters : (int) number of iterations to run gradient descent
-    Returns
-      w : (ndarray): Shape (1,) Updated values of parameters of the model after
-          running gradient descent
-      b : (scalar) Updated value of parameter of the model after
-          running gradient descent
-      J_history : (ndarray): Shape (num_iters,) J at each iteration,
-          primarily for graphing later
-    """
     
-    m = len(y)  # Número de ejemplos de entrenamiento
-    w = w_in  # Inicialización de w
-    b = b_in  # Inicialización de b
-    J_history = np.zeros(num_iters)  # Inicialización del historial de costos
+    #Inicializacion
+    w = w_in
+    b = b_in
+
+    # Inicializar el array que almacenara los costes que se vayan calculando
+    # Todos sus elementos empiezan siendo 0
+    J_history = np.zeros(num_iters)
+
+    # Numero de puntos
+    m = len(y)
     
     for i in range(num_iters):
-        # Calcular el costo actual
-        # Almacenar el costo actual en el historial de costos
+
+        # Guardar el coste de esta iteracion
         J_history[i] = cost_function(x, y, w, b)
         
-        # Calcular el gradiente actual
+        # Calcular los gradientes
         dj_dw, dj_db = gradient_function(x, y, w, b)
         
-        # Actualizar los parámetros w y b usando el descenso de gradiente
+        # Cambiar w y b, para que se acerquen mas a la recta real con cada iteracion
         w -= alpha * dj_dw
         b -= alpha * dj_db
     
