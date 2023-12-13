729686d3f4092366557be5e77203f40d74cbec38
NachoDelCastillo <nachodelcastillorubio@gmail.com>
2023-12-13 20:21:17 +0100

    ejercicios 1 y 2

--- Practica5/ann.py
+++ Practica5/ann.py
@@ -1,8 +1,27 @@
 import numpy as np
 
-from utils import sig, sig_gradient
+def forward(theta_list, a_in):
+    m = a_in.shape[0]
+    a_in = np.hstack([np.ones((m, 1)), a_in])
+    activations = [a_in]
+    zs = []
 
-def cost(theta1, theta2, X, y, lambda_):
+    for i in range(len(theta_list)):
+        z = np.dot(activations[-1], theta_list[i].T)
+
+		# Calcular el sig
+        a = 1 / (1 + np.exp(-z))
+
+        if (i < len(theta_list) - 1):
+            a = np.hstack([np.ones((m, 1)), a])
+
+        activations.append(a)
+        zs.append(z)
+
+    return activations, zs
+
+def costL2(theta_list, X, y, lambda_):
+    
 	"""
 	Compute cost for 2-layer neural network. 
 
@@ -32,128 +51,150 @@ def cost(theta1, theta2, X, y, lambda_):
 		The computed value for the cost function. 
 
 	"""
-	
+
 	m = len(y)
-	activations, zs = feedForward(theta_list, X)
+	activations, zs = forward(theta_list, X)
 	predictions = activations[-1]
-	
+
 	J = (-1 / m) * np.sum(y * np.log(predictions) + (1 - y) * np.log(1 - predictions))
 
+	reg_term = 0
+	for theta in theta_list:
+		reg_term += np.sum(theta[:, 1:]**2)
+	reg_term *= (lambda_ / (2 * m))
+	J += reg_term
 
 	return J
 
+# def forward(theta_list, a_in):
 
+#     m = a_in.shape[0]
+#     a_in = np.hstack([np.ones((m, 1)), a_in])
+#     activations = [a_in]
+#     zs = []
+    
+#     for i in range(len(theta_list)):
+#         z = np.dot(activations[-1], theta_list[i].T)
+#         a = 1/(1+np.exp(-z))
+        
+#         if (i < len(theta_list) - 1):
+#             a = np.hstack([np.ones((m, 1)), a])
+            
+#         activations.append(a)
+#         zs.append(z)
+        
+#     return activations, zs
+
+
+# def costL2(theta_list, X, y, lambda_):
+# 	"""
+# 	Compute cost for 2-layer neural network. 
+
+# 	Parameters
+# 	----------
+# 	theta1 : array_like
+# 		Weights for the first layer in the neural network.
+# 		It has shape (2nd hidden layer size x input size + 1)
+
+# 	theta2: array_like
+# 		Weights for the second layer in the neural network. 
+# 		It has shape (output layer size x 2nd hidden layer size + 1)
+
+# 	X : array_like
+# 		The inputs having shape (number of examples x number of dimensions).
+
+# 	y : array_like
+# 		1-hot encoding of labels for the input, having shape 
+# 		(number of examples x number of labels).
+
+# 	lambda_ : float
+# 		The regularization parameter. 
+
+# 	Returns
+# 	-------
+# 	J : float
+# 		The computed value for the cost function. 
+
+# 	"""
+
+    
+# 	m = len(y)
+# 	activations, zs = forward(theta_list, X)
+# 	predictions = activations[-1]
+    
+# 	J = (-1 / m) * np.sum(y * np.log(predictions) + (1 - y) * np.log(1 - predictions))
+    
+# 	reg_term = 0
+# 	for theta in theta_list:
+# 		reg_term += np.sum(theta[:, 1:]**2)
+# 	reg_term *= (lambda_ / (2 * m))
+# 	J += reg_term
+    
+# 	return J
+
+
+def sig(z):
+    return 1/(1+np.exp(-z))
 
 def backprop(theta1, theta2, X, y, lambda_):
-	"""
-	Compute cost and gradient for 2-layer neural network. 
-
-	Parameters
-	----------
-	theta1 : array_like
-		Weights for the first layer in the neural network.
-		It has shape (2nd hidden layer size x input size + 1)
-
-	theta2: array_like
-		Weights for the second layer in the neural network. 
-		It has shape (output layer size x 2nd hidden layer size + 1)
-
-	X : array_like
-		The inputs having shape (number of examples x number of dimensions).
-
-	y : array_like
-		1-hot encoding of labels for the input, having shape 
-		(number of examples x number of labels).
-
-	lambda_ : float
-		The regularization parameter. 
-
-	Returns
-	-------
-	J : float
-		The computed value for the cost function. 
-
-	grad1 : array_like
-		Gradient of the cost function with respect to weights
-		for the first layer in the neural network, theta1.
-		It has shape (2nd hidden layer size x input size + 1)
-
-	grad2 : array_like
-		Gradient of the cost function with respect to weights
-		for the second layer in the neural network, theta2.
-		It has shape (output layer size x 2nd hidden layer size + 1)
-
-	"""
-
-	m = X.shape[0]
-	n_labels = theta_list[-1].shape[0]
-
-	activations, zs = feedForward(theta_list, X)
-	a_last = activations[-1]
-
-	J = cost_regL2(theta_list, X, y, lambda_)
-
-	deltas = [a_last - y]
-	for i in range(len(theta_list) - 1, 0, -1):
-		delta = np.dot(deltas[0], theta_list[i][:, 1:]) * sig_gradient(zs[i - 1])
-		deltas.insert(0, delta)
-
-	grads = [np.dot(d.T, a) / m for d, a in zip(deltas, activations[:-1])]
-
-	for i in range(len(grads)):
-		grads[i][:, 1:] += (lambda_ / m) * theta_list[i][:, 1:]
-
-	return J, grads
-
-
-	return (J, grad1, grad2)
-
-def feedForward(theta_list, a_in):
+    """
+    Compute cost and gradient for 2-layer neural network.
     
-    m = a_in.shape[0]
-    a_in = np.hstack([np.ones((m, 1)), a_in])
-    activations = [a_in]
-    zs = []
+    Parameters
+    ----------
+    theta1 : array_like
+        Weights for the first layer in the neural network.
+        It has shape (2nd hidden layer size x input size + 1)
     
-    for i in range(len(theta_list)):
-        z = np.dot(activations[-1], theta_list[i].T)
-        a = sig(z)
-        
-        if (i < len(theta_list) - 1):
-            a = np.hstack([np.ones((m, 1)), a])
-            
-        activations.append(a)
-        zs.append(z)
-        
-    return activations, zs
+    theta2 : array_like
+        Weights for the second layer in the neural network.
+        It has shape (output layer size x 2nd hidden layer size + 1)
+    
+    X : array_like
+        The inputs having shape (number of examples x number of dimensions).
+    
+    y : array_like
+        1-hot encoding of labels for the input, having shape
+        (number of examples x number of labels).
+    
+    lambda_ : float
+        The regularization parameter.
+    
+    Returns
+    -------
+    J : float
+        The computed value for the cost function.
+    
+    grad1 : array_like
+        Gradient of the cost function with respect to weights
+        for the first layer in the neural network, theta1.
+        It has shape (2nd hidden layer size x input size + 1)
+    
+    grad2 : array_like
+        Gradient of the cost function with respect to weights
+        for the second layer in the neural network, theta2.
+        It has shape (output layer size x 2nd hidden layer size + 1)
+    """
+    theta_list = [theta1, theta2]
+    m = X.shape[0]
+    n_labels = theta2.shape[0]
 
+    activations, zs = forward(theta_list, X)
+    a_last = activations[-1]
 
-def predict(theta_list, a_in):
-    
-    activations, zs = feedForward(theta_list, a_in)
-    
-    p = np.argmax(activations[-1], axis=1)
+    J = costL2(theta_list, X, y, lambda_)
 
-    return p
+    deltas = [a_last - y]
+    for i in range(len(theta_list) - 1, 0, -1):
+        # Calcular el gradiente
+        sigGrad = sig(zs[i - 1]) * (1 - sig(zs[i - 1]))
+        delta = np.dot(deltas[0], theta_list[i][:, 1:]) * sigGrad
+        deltas.insert(0, delta)
 
-def predict_without_feedforward(out_a):
-    
-    p = np.argmax(out_a, axis=1)
+    grad1 = np.dot(deltas[0].T, activations[0]) / m
+    grad2 = np.dot(deltas[1].T, activations[1]) / m
 
-    return p
+    grad1[:, 1:] += (lambda_ / m) * theta1[:, 1:]
+    grad2[:, 1:] += (lambda_ / m) * theta2[:, 1:]
 
-def cost_regL2(theta_list, X, y, lambda_):
-    
-    m = len(y)
-    activations, zs = feedForward(theta_list, X)
-    predictions = activations[-1]
-    
-    J = (-1 / m) * np.sum(y * np.log(predictions) + (1 - y) * np.log(1 - predictions))
-    
-    reg_term = 0
-    for theta in theta_list:
-        reg_term += np.sum(theta[:, 1:]**2)
-    reg_term *= (lambda_ / (2 * m))
-    J += reg_term
-    
-    return J
+    return J, grad1, grad2
